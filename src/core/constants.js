8e346706a8a073523b4d9bab086c6345400426d5
houseburgertx-beep <houseburgertx@gmail.com>
2026-07-29 02:56:43 -0300

    Corrigir área segura do iPhone

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,6 +1,6 @@
 export const APP = Object.freeze({
   name: "Gestão de Folgas",
-  version: "6.1.5-firebase-github",
+  version: "6.1.6-firebase-github",
   profiles: {
     admin: "Administrador",
     manager: "Responsável de loja",
