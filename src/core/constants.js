c96cf4dc0fa9e58cd2abd3f62eecd66f84c075be
houseburgertx-beep <houseburgertx@gmail.com>
2026-07-30 13:17:09 -0300

    Ocultar aviso de sala expirada no House Link

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,6 +1,6 @@
 export const APP = Object.freeze({
   name: "Gestão de Folgas",
-  version: "6.2.0-firebase-github",
+  version: "6.2.1-firebase-github",
   profiles: {
     admin: "Administrador",
     manager: "Responsável de loja",
