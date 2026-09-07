3de076514256fcc330f72dba5066480bd433ee02
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-07 12:37:43 -0300

    Criar experiência mobile de jornada com painel visual e escala

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "house-folgas-v6.4.0";
+const CACHE_NAME = "house-folgas-v6.5.0";
 const APP_BASE = new URL("./", self.location.href);
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./manifest.webmanifest?v=6.4.0",
+  "./manifest.webmanifest?v=6.5.0",
   "./apple-touch-icon-6.1.5.png",
   "./apple-touch-icon.png",
   "./icons/app-icon-192.png",
